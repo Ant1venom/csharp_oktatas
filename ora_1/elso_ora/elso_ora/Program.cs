55414af27f6ef97093d686e8de9577de6a18a5e3
Ant1venom <botadanieltamas@gmail.com>
2022-12-05 14:34:58 +0100

    elso ora anyaga

---
+++ ora_1/elso_ora/elso_ora/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace elso_ora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 1000000000;
+            int prev = 0;
+            for (int i = 0; i < max; i++)
+            {
+                var newClass = new SomeClass();
+                SomeClass newClass2 = new();
+                if (SomeClass.NumberOfClasses < prev)
+                {
+                    break;
+                }
+                prev = SomeClass.NumberOfClasses;
+            }
+
+            Console.WriteLine(SomeClass.NumberOfClasses);
+        }
+    }
+}
---
+++ ora_1/elso_ora/elso_ora/some_class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace elso_ora
+{
+
+    class SomeClass
+    {
+
+        public static int NumberOfClasses;
+
+        public SomeClass()
+        {
+            NumberOfClasses++;
+        }
+
+        ~SomeClass()
+        {
+            NumberOfClasses--;
+        }
+    }
+}
